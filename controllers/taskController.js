cbf62e01b9a168b56a31256ece39c4b41ccc08d4
RafalJackiewicz <jackiewicz.rafall@gmail.com>
2023-09-20 22:55:48 +0200

    Aktualizacja projektu 20.09.2023

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -24,6 +24,19 @@ const getAllTasks = async (req, res) => {
     });
   }
 };
+
+const deleteTask = async (req, res) => {
+  const idTask = req.body.id;
+  console.log('id', idTask);
+  const q = 'DELETE FROM `tasks` WHERE `id`=:idTask';
+  await pool.execute(q, {
+    idTask,
+  });
+  res.json({
+    status: 'success',
+  });
+};
+
 const addNewTask = async (req, res) => {
   const newTask = req.body.title;
   const q = 'INSERT INTO `tasks` VALUES(:id,:taskName,0)';
@@ -40,4 +53,5 @@ const addNewTask = async (req, res) => {
 module.exports = {
   getAllTasks,
   addNewTask,
+  deleteTask,
 };
--- routers/task.js
+++ routers/task.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const { Router } = require('express');
-const { getAllTasks, addNewTask } = require('../controllers/taskController');
+const {
+  getAllTasks,
+  addNewTask,
+  deleteTask,
+} = require('../controllers/taskController');
 
 // const { ValidationError } = require('../utils/errors');
 
 const taskRouter = Router();
 
 taskRouter.route('/').get(getAllTasks);
+taskRouter.route('/delete').delete(deleteTask);
 taskRouter.route('/add-task').post(addNewTask);
-// taskRouter.route('/editing').post();
-// taskRouter.route('/delete').detele()
 
 module.exports = {
   taskRouter,
